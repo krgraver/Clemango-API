49d5b7614a704a21d374aec16a57f5427270367a
krgraver <krgraver@gmail.com>
2017-01-29 20:28:01 -0600

    removed passport modules

--- index.js
+++ index.js
@@ -24,6 +24,6 @@ app.post('/uploads/getOne', uploadController.getUpload);
 app.post('/uploads/post', uploadController.postUpload);
 
 
-app.listen('3001', function() {
+app.listen('5000', function() {
 	console.log("clemango-server is running");
 });
