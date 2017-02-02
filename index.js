28908ad58c96324d8f0e8d1998e88cdee67e1cc5
krgraver <krgraver@gmail.com>
2017-02-01 21:52:43 -0600

    changed app folder to server

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var expressJWT = require('express-jwt');
+var fs = require('fs');
+var AWS = require('aws-sdk');
 
-var userController = require('./app/controllers/user-controller');
-var uploadController = require('./app/controllers/upload-controller');
+var userController = require('./server/controllers/user-controller');
+var uploadController = require('./server/controllers/upload-controller');
 
 var app = express();
 
-var constant = require('./app/config/constants')
+var constant = require('./server/config/constants');
 mongoose.connect(constant.MONGODB_URI);
 
 app.use(bodyParser.json());
@@ -26,5 +28,5 @@ app.post('/uploads/post', uploadController.postUpload);
 
 
 app.listen(process.env.PORT || '5000', function() {
-	console.log("clemango-server is running");
+	console.log("clemango api is running");
 });
