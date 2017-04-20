d28660299e3f97c42061f604618d002a1c2b2e54
krgraver <krgraver@gmail.com>
2017-04-19 21:51:43 -0500

    changed header to one origin

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
     res.setHeader('Access-Control-Allow-Origin', 'https://clemango.herokuapp.com');
-    res.setHeader('Access-Control-Allow-Origin', 'http://clemango.herokuapp.com');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
