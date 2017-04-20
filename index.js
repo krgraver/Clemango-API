e8ea2258977d5bb9dc08763efcfe5ac5a600f254
krgraver <krgraver@gmail.com>
2017-04-20 13:40:23 -0500

    now allow multiple domains in header

--- index.js
+++ index.js
@@ -16,9 +16,12 @@ mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI);
 
 app.use(function (req, res, next) {
+	var allowedOrigins = ['https://clemango.herokuapp.com', 'http://clemango.herokuapp.com', 'http://localhost:3000'];
+	var origin = req.headers.origin;
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://clemango.herokuapp.com');
+	if (allowedOrigins.indexOf(origin) > -1) {
+		res.setHeader('Access-Control-Allow-Origin', origin);
+	}
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
